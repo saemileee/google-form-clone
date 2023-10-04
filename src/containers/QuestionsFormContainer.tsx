a3f648e4c7dd63189d7893022f3c296df60cf375
saem <saemilee01@gmail.com>
2023-10-05 01:21:09 +0900

    feat: 전역 question 상태 매핑

--- src/containers/QuestionsFormContainer.tsx
+++ src/containers/QuestionsFormContainer.tsx
@@ -1,14 +1,20 @@
+import {useSelector} from 'react-redux';
 import Description from '../components/FormFields/Description';
 import Question from '../components/FormFields/Question';
 import Title from '../components/FormFields/Title';
+import {RootState} from '../store/store';
 
 const QuestionsFormContainer = () => {
+    const questionForm = useSelector((state: RootState) => state.questionForm);
+    const {questions} = questionForm;
     return (
         <>
             <h1>QuestionsFormContainer</h1>
             <Title />
             <Description />
-            <Question />
+            {questions.map((_, idx) => (
+                <Question key={idx} idx={idx} />
+            ))}
         </>
     );
 };
