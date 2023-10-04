ebcfc20b2e6875141edb5db9b35f200c857185ad
saem <saemilee01@gmail.com>
2023-10-04 19:30:52 +0900

    feat: Question 컴포넌트 관련 상수 추가

--- src/constants/Form.ts
+++ src/constants/Form.ts
@@ -1,7 +1,11 @@
+import {QuestionType} from '../interface/Form';
+
 export const DEFAULT_VALUES = {
     TITLE: 'Untitled form',
+    QUESTION_TYPE: QuestionType.multipleChoice,
 };
 
 export const PLACEHOLDERS = {
     DESCRIPTION: 'Form description',
+    QUESTION: 'Question',
 };
