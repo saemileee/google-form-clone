eceee2401c987986ee62df6130742b805cd6254c
saem <saemilee01@gmail.com>
2023-10-05 01:20:01 +0900

    feat: question 타이틀, 옵션 디폴트 값 추가

--- src/constants/Form.ts
+++ src/constants/Form.ts
@@ -2,7 +2,9 @@ import {QuestionType} from '../interface/Form';
 
 export const DEFAULT_VALUES = {
     TITLE: 'Untitled form',
+    QUESTION_TITLE: 'Untitled Question',
     QUESTION_TYPE: QuestionType.multipleChoice,
+    QUESTION_OPTION: 'Option 1',
 };
 
 export const PLACEHOLDERS = {
