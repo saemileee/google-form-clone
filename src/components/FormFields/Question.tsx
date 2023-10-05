30f82674fd56a52c554c8d0b1fbf76a388ddd424
saem <saemilee01@gmail.com>
2023-10-06 02:55:54 +0900

    refactor: custom hook의 handler로 상태 조작했던 부분 dispatch로 변경

--- src/components/FormFields/Question.tsx
+++ src/components/FormFields/Question.tsx
@@ -1,37 +1,54 @@
 import {DEFAULT_VALUES, PLACEHOLDERS} from '../../constants/Form';
 import * as I from '../../interface/Form';
-import useQuestionForm from '../../hooks/useQuestionForm';
 import {RootState} from '../../store/store';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
+import {
+    addQuestionOption,
+    changeOptionValue,
+    resortQuestionOptions,
+    changeQuestionTitle,
+    deleteQuestion,
+    duplicateQuestion,
+    removeQuestionOption,
+    toggleRequired,
+    changeQuestionType,
+} from '../../features/questionFormSlice';
+import useTextInputField from '../../hooks/useTextInputField';
+import useSortableDragNDrop from '../../hooks/useSortableDragNDrop';
 
 const MIN_OPTION_LENGTH = 1;
-const Question = ({idx}: {idx: number}) => {
-    const formData = useSelector((state: RootState) => state.questionForm.questions[idx]);
+const Question = ({questionIdx}: {questionIdx: number}) => {
+    const formData = useSelector((state: RootState) => state.questionForm.questions[questionIdx]);
+    const dispatch = useDispatch();
 
-    const {titleHandlers, changeType, optionHandlers, toggleRequired, sectionHandlers} =
-        useQuestionForm();
-
-    const {duplicateSection, deleteSection} = sectionHandlers;
     const {title, type, options, isRequired} = formData;
 
-    const {isTitleFocused, changeTitle, focusTitle, blurTitle} = titleHandlers;
-    const {addOption, removeOption, changeOptionValue, dragNDropOption} = optionHandlers;
-    const {isDraggable, startDrag, enterTarget, endDrag, mouseDown, mouseUp} = dragNDropOption;
+    const {isFocused, onFocus, onBlur} = useTextInputField();
+
+    const dragNDropOption = useSortableDragNDrop(options);
+    const {isDraggable, startDrag, enterTarget, getResortedList, mouseDown, mouseUp} =
+        dragNDropOption;
 
     return (
         <section>
             <div>
                 <input
-                    style={isTitleFocused ? {outlineColor: 'red'} : undefined}
+                    style={isFocused ? {outlineColor: 'red'} : undefined}
                     type='text'
                     value={title}
                     placeholder={PLACEHOLDERS.QUESTION}
-                    onChange={changeTitle}
-                    onFocus={focusTitle}
-                    onBlur={blurTitle}
+                    onChange={e => {
+                        const value = e.target.value;
+                        dispatch(changeQuestionTitle({questionIdx, value}));
+                    }}
+                    onFocus={onFocus}
+                    onBlur={onBlur}
                 />
                 <select
-                    onChange={e => changeType(e.target.value)}
+                    onChange={e => {
+                        const value = e.target.value;
+                        dispatch(changeQuestionType({questionIdx, value}));
+                    }}
                     defaultValue={DEFAULT_VALUES.QUESTION_TYPE}
                 >
                     {Object.entries(I.QuestionType).map(([key, value]) => (
@@ -52,9 +69,12 @@ const Question = ({idx}: {idx: number}) => {
                             <div
                                 key={`option-${optionIdx}`}
                                 draggable={isDraggable}
-                                onDragStart={() => startDrag(idx)}
-                                onDragEnter={() => enterTarget(idx)}
-                                onDragEnd={endDrag}
+                                onDragStart={() => startDrag(optionIdx)}
+                                onDragEnter={() => enterTarget(optionIdx)}
+                                onDragEnd={() => {
+                                    const options = getResortedList();
+                                    dispatch(resortQuestionOptions({questionIdx, options}));
+                                }}
                                 onDragOver={e => e.preventDefault()}
                             >
                                 <button onMouseDown={mouseDown} onMouseUp={mouseUp}>
@@ -66,29 +86,46 @@ const Question = ({idx}: {idx: number}) => {
                                 <input
                                     type='text'
                                     value={value}
-                                    onChange={e =>
-                                        changeOptionValue(idx, optionIdx, e.target.value)
-                                    }
+                                    onChange={e => {
+                                        const value = e.target.value;
+                                        dispatch(
+                                            changeOptionValue({questionIdx, optionIdx, value})
+                                        );
+                                    }}
                                 />
                                 {options.length > MIN_OPTION_LENGTH ? (
-                                    <button onClick={() => removeOption(idx, optionIdx)}>X</button>
+                                    <button
+                                        onClick={() =>
+                                            dispatch(removeQuestionOption({questionIdx, optionIdx}))
+                                        }
+                                    >
+                                        X
+                                    </button>
                                 ) : (
-                                    idx >= MIN_OPTION_LENGTH && (
-                                        <button onClick={() => removeOption(idx, optionIdx)}>
+                                    questionIdx >= MIN_OPTION_LENGTH && (
+                                        <button
+                                            onClick={() =>
+                                                dispatch(
+                                                    removeQuestionOption({questionIdx, optionIdx})
+                                                )
+                                            }
+                                        >
                                             X
                                         </button>
                                     )
                                 )}
                             </div>
                         ))}
-                        <button onClick={() => addOption(idx)}>Add option</button>
+                        <button onClick={() => dispatch(addQuestionOption(questionIdx))}>
+                            Add option
+                        </button>
                     </div>
                 )}
             </div>
             <div>
-                <button onClick={() => duplicateSection(idx)}>Duplicate</button>
-                <button onClick={() => deleteSection(idx)}>Delete</button>
-                <div onClick={toggleRequired}>
+                <button onClick={() => dispatch(duplicateQuestion(questionIdx))}>Duplicate</button>
+                <button onClick={() => dispatch(deleteQuestion(questionIdx))}>Delete</button>
+                <div onClick={() => dispatch(toggleRequired({questionIdx}))}>
                     Required <button>{isRequired.toString()}</button>
                 </div>
             </div>
