221d8d389de5d28fe7c3e0c3eae7f7b25a3d987a
saem <saemilee01@gmail.com>
2023-10-06 03:40:31 +0900

    refactor: QUESTION_OPTIOIN 값 변경

--- src/constants/Form.ts
+++ src/constants/Form.ts
@@ -1,17 +1,17 @@
-import {Option, FormDescription, FormTitle, QuestionType} from '../interface/Form';
+import {OptionType, FormDescription, FormTitle, QuestionType} from '../interface/Form';
 
 interface DefaultValues {
     TITLE: FormTitle;
     QUESTION_TITLE: FormDescription;
     QUESTION_TYPE: QuestionType;
-    QUESTION_OPTION: Option;
+    QUESTION_OPTION: OptionType;
 }
 
 export const DEFAULT_VALUES: DefaultValues = {
     TITLE: 'Untitled form',
     QUESTION_TITLE: 'Untitled Question',
     QUESTION_TYPE: 'MultipleChoice',
-    QUESTION_OPTION: 'Option 1',
+    QUESTION_OPTION: 'Option',
 };
 
 export const PLACEHOLDERS = {
