445f4828359c7a63d5469da57278c297833bca96
saem <saemilee01@gmail.com>
2023-10-06 18:24:59 +0900

    feat: form에 범용적으로 사용될 스타일 컴포넌트 추가

---
+++ src/styles/Form.ts
@@ -0,0 +1,103 @@
+import styled from 'styled-components';
+
+interface StyledGeneralFormContainerProps {
+    isSelected?: boolean;
+}
+export const StyledGeneralFormContainer = styled.div<StyledGeneralFormContainerProps>`
+    overflow: hidden;
+    position: relative;
+    border: 1px solid lightgrey;
+    border-radius: 8px;
+    background-color: white;
+    &:hover {
+        .question-form-drag-button {
+            visibility: visible;
+        }
+    }
+    input {
+        &:hover {
+            border-bottom: ${props => (props.isSelected ? '1px solid lightgrey' : '')};
+            margin-bottom: ${props => (props.isSelected ? '-1px' : '')};
+        }
+        &:focus {
+            border-bottom: 2px solid blue;
+            margin-bottom: -2px;
+        }
+    }
+`;
+
+export const StyledGeneralFormWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+    box-sizing: border-box;
+    padding: 18px;
+`;
+
+export const StyledTextInput = styled.input`
+    padding: 8px 0 8px 0;
+    width: 100%;
+    border: none;
+    outline: none;
+    font-size: 12pt;
+`;
+
+export const StyledTitleInput = styled(StyledTextInput)`
+    padding: 12px 0 12px 0;
+    font-size: 24pt;
+    font-weight: 500;
+`;
+
+export const StyledQuestionTitleInput = styled(StyledTextInput)`
+    font-weight: 400;
+    &.selected {
+        padding: 14px;
+        background-color: lightgrey;
+        border-bottom: 1px solid blue;
+    }
+`;
+
+interface StyledDragButtonWProps {
+    selected?: boolean;
+}
+
+export const StyledDragButton = styled.button<StyledDragButtonWProps>`
+    padding: 4px 0 4px 0;
+    color: grey;
+    cursor: all-scroll;
+    visibility: ${props => (props.selected ? 'visible' : 'hidden')};
+`;
+
+StyledDragButton.shouldForwardProp = prop => prop !== 'selected';
+
+export const StyledDragButtonW = styled(StyledDragButton)`
+    width: 100%;
+`;
+
+export const StyledDragButtonH = styled(StyledDragButton)<StyledDragButtonWProps>`
+    height: 100%;
+`;
+
+interface StyledMenuButtonProps {
+    name: string;
+}
+export const StyledMenuButton = styled.button<StyledMenuButtonProps>`
+    position: relative;
+    width: 48px;
+    height: 48px;
+    border-radius: 30px;
+    &:hover {
+        visibility: visible;
+        background-color: lightgrey;
+        &::after {
+            visibility: hidden;
+            content: '${props => props.name}';
+            position: absolute;
+            bottom: -24px;
+            padding: 4px;
+            border-radius: 4px;
+            background-color: grey;
+            transform: translateX(-24px);
+        }
+    }
+`;
