7eb4bb5204e48939cdbbdae3862244541cf133da
saem <saemilee01@gmail.com>
2023-10-07 12:19:29 +0900

    feat: 로컬스토리지 조작 유틸함수 및 스토어 추가

---
+++ src/store/localStorage.ts
@@ -0,0 +1,3 @@
+import {LocalStorage} from '../utils/localStorage';
+
+export const formStateStorage = new LocalStorage('formStateStorage');
---
+++ src/utils/localStorage.ts
@@ -0,0 +1,24 @@
+export class LocalStorage<T> {
+  key: string;
+  constructor(key: string) {
+    this.key = key;
+  }
+
+  getItem() {
+    try {
+      return JSON.parse(localStorage.getItem(this.key)!);
+    } catch (e) {
+      console.error('파싱될 수 없는 value 값입니다. 스토리지를 리셋합니다.');
+      this.setItem('');
+    }
+  }
+
+  setItem(value: T | string) {
+    try {
+      return localStorage.setItem(this.key, JSON.stringify(value));
+    } catch (e) {
+      console.error('용량을 초과했습니다. 스토리지를 리셋합니다.');
+      this.setItem('');
+    }
+  }
+}
