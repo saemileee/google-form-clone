48099f168005c8385982f6c41988879142d4bf63
saem <saemilee01@gmail.com>
2023-10-07 12:34:06 +0900

    design: container, wrapper 컴포넌트 적용

--- src/containers/SurveyPreviewFormContainer.tsx
+++ src/containers/SurveyPreviewFormContainer.tsx
@@ -1,15 +1,24 @@
 import SurveyPreviewDescription from '../components/SurveyPreviewForm/Description';
 import SurveyPreviewTitle from '../components/SurveyPreviewForm/Title';
 import {formStateStorage} from '../store/localStorage';
+import {
+  StyledGeneralFormContainer,
+  StyledGeneralFormWrapper,
+  StyledSurveyContainer,
+} from '../styles/Form';
 
 const PreviewContainer = () => {
   const formData = formStateStorage.getItem();
   const {title, description} = formData;
   return (
-    <>
-      <SurveyPreviewTitle title={title} />
-      <SurveyPreviewDescription description={description} />
-    </>
+    <StyledSurveyContainer>
+      <StyledGeneralFormContainer>
+        <StyledGeneralFormWrapper>
+          <SurveyPreviewTitle title={title} />
+          <SurveyPreviewDescription description={description} />
+        </StyledGeneralFormWrapper>
+      </StyledGeneralFormContainer>
+    </StyledSurveyContainer>
   );
 };
 
