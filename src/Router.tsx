cced87a3c2c273d1099098ee391a63a6efa894ee
saem <saemilee01@gmail.com>
2023-10-08 03:16:58 +0900

    feat: result 페이지 임포트 경로 재설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,7 +2,7 @@ import {createBrowserRouter} from 'react-router-dom';
 import App from './App';
 import {default as Main} from './containers/SurveyPostFormContainer';
 import {default as Preview} from './containers/SurveyPreviewFormContainer';
-import {default as Result} from './containers/SurveyPreviewFormContainer';
+import {default as Result} from './containers/SurveyResultContainer';
 
 import Header from './components/Header';
 
