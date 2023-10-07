8e3f44839f5d163433b3e4f5d7743e8c46de57bf
saem <saemilee01@gmail.com>
2023-10-08 04:26:53 +0900

    refactor: 불필요한 훅 사용 제거

--- src/components/SurveyPostForm/Title.tsx
+++ src/components/SurveyPostForm/Title.tsx
@@ -1,6 +1,4 @@
 import {useDispatch, useSelector} from 'react-redux';
-import {DEFAULT_VALUES} from '../../constants/Form';
-import useTextInputField from '../../hooks/useTextInputField';
 import {changeTitle} from '../../features/questionFormSlice';
 import {RootState} from '../../store/store';
 import {StyledTitleInput} from '../../styles/Form';
@@ -9,19 +7,14 @@ const Title = () => {
   const title = useSelector((state: RootState) => state.questionForm.title);
   const dispatch = useDispatch();
 
-  const {isFocused, onFocus, onBlur} = useTextInputField(DEFAULT_VALUES.TITLE);
-
   return (
     <StyledTitleInput
-      style={isFocused ? {outlineColor: 'red'} : undefined}
       type='text'
       value={title}
       onChange={e => {
         const value = e.target.value;
         dispatch(changeTitle({value}));
       }}
-      onFocus={onFocus}
-      onBlur={onBlur}
     />
   );
 };
