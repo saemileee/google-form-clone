b3bcc91c94674c4d9cdb003045cc55b3f9797545
샘 <68241138+saemileee@users.noreply.github.com>
2023-10-10 05:47:31 +0900

    refactor: non-null assertion operator 수정

--- src/features/surveyPreviewFormSlice.ts
+++ src/features/surveyPreviewFormSlice.ts
@@ -3,24 +3,22 @@ import {AnswerDropDown, AnswerMultipleChoice, PreviewQuestionForm} from '../inte
 import {formStateStorage} from '../store/localStorage';
 import {surveyPostFormToPrevFormState} from '../utils/formStateConverter';
 import {OTHER_IDX, QUESTION_TYPES} from '../constants/Form';
-
-// 임시 이니셜 상태
-const initialQuestionState = {
-  isSelected: false,
-  title: '',
-  type: QUESTION_TYPES.checkboxes,
-  options: [''],
-  isOtherSelected: false,
-  isRequired: false,
-};
+import {initialState as postFormInitialState} from './surveyPostSlice';
 
 const initialState: PreviewQuestionForm = surveyPostFormToPrevFormState(
-  formStateStorage.getItem() || {
-    title: '',
-    description: '',
-    questions: [initialQuestionState],
-  }
+  formStateStorage.getItem() || postFormInitialState
 );
+
+const initialMultipleChoiceAnswer = {
+  selectedOptionIndex: null,
+  other: null,
+};
+
+const initialCheckboxesAnswer = {
+  selectedOptionIndexes: [],
+  other: null,
+};
+
 const surveyPreviewFormSlice = createSlice({
   name: 'surveyPreviewForm',
   initialState,
@@ -32,69 +30,95 @@ const surveyPreviewFormSlice = createSlice({
       }
     ) => {
       const {questionIdx, selectedIdx} = action.payload;
-      const currentSelect = state.questions[questionIdx].answer.multipleChoice!.selectedOptionIndex;
+
+      const currentSelect = (
+        state.questions[questionIdx].answer.multipleChoice || initialMultipleChoiceAnswer
+      ).selectedOptionIndex;
+
       if (selectedIdx === currentSelect) {
-        state.questions[questionIdx].answer.multipleChoice!.selectedOptionIndex = null;
-      } else {
-        state.questions[questionIdx].answer.multipleChoice!.selectedOptionIndex = selectedIdx;
+        (
+          state.questions[questionIdx].answer.multipleChoice || initialMultipleChoiceAnswer
+        ).selectedOptionIndex = null;
+        return;
       }
+
+      (
+        state.questions[questionIdx].answer.multipleChoice || initialMultipleChoiceAnswer
+      ).selectedOptionIndex = selectedIdx;
     },
+
     toggleCheckboxOption: (
       state,
       action: {payload: {questionIdx: number; selectedIdx: number | typeof OTHER_IDX}}
     ) => {
       const {questionIdx, selectedIdx} = action.payload;
 
-      const currentSelects = state.questions[questionIdx].answer.checkboxes!.selectedOptionIndexes;
+      const currentSelects = (
+        state.questions[questionIdx].answer.checkboxes || initialCheckboxesAnswer
+      ).selectedOptionIndexes;
 
       if (currentSelects.includes(selectedIdx)) {
-        state.questions[questionIdx].answer.checkboxes!.selectedOptionIndexes =
-          currentSelects.filter(select => select !== selectedIdx);
+        (
+          state.questions[questionIdx].answer.checkboxes || initialCheckboxesAnswer
+        ).selectedOptionIndexes = currentSelects.filter(select => select !== selectedIdx);
       } else {
-        state.questions[questionIdx].answer.checkboxes!.selectedOptionIndexes = [
-          ...currentSelects,
-          selectedIdx,
-        ];
+        (
+          state.questions[questionIdx].answer.checkboxes || initialCheckboxesAnswer
+        ).selectedOptionIndexes = [...currentSelects, selectedIdx];
       }
     },
+
     selectDropDownOption: (
       state,
       action: {
         payload: {questionIdx: number; selectedIdx: AnswerDropDown['selectedOptionIndex']};
       }
     ) => {
-      console.info(action.payload);
       const {questionIdx, selectedIdx} = action.payload;
-      state.questions[questionIdx].answer.dropDown!.selectedOptionIndex = selectedIdx;
+      (
+        state.questions[questionIdx].answer.dropDown || {
+          selectedOptionIndex: null,
+        }
+      ).selectedOptionIndex = selectedIdx;
     },
 
     changeTextAnswer: (state, action: {payload: {questionIdx: number; value: string}}) => {
       const {questionIdx, value} = action.payload;
-      if (state.questions[questionIdx].answer.shortAnswer) {
-        state.questions[questionIdx].answer.shortAnswer!.answer = value;
-      } else if (state.questions[questionIdx].answer.paragraph) {
-        state.questions[questionIdx].answer.paragraph!.answer = value;
+      const initialTextAnswer = {answer: ''};
+      if (state.questions[questionIdx].layout.type === QUESTION_TYPES.shortAnswer) {
+        (state.questions[questionIdx].answer.shortAnswer || initialTextAnswer).answer = value;
+      } else if (state.questions[questionIdx].layout.type === QUESTION_TYPES.paragraph) {
+        (state.questions[questionIdx].answer.paragraph || initialTextAnswer).answer = value;
       }
     },
 
     typeOtherOption: (state, action: {payload: {questionIdx: number; value: string}}) => {
       const {questionIdx, value} = action.payload;
-      if (state.questions[questionIdx].answer.multipleChoice) {
-        state.questions[questionIdx].answer.multipleChoice!.other = value;
+      if (state.questions[questionIdx].layout.type === QUESTION_TYPES.multipleChoice) {
+        (state.questions[questionIdx].answer.multipleChoice || initialMultipleChoiceAnswer).other =
+          value;
 
-        const currentSelected =
-          state.questions[questionIdx].answer.multipleChoice!.selectedOptionIndex;
+        const currentSelected = (
+          state.questions[questionIdx].answer.multipleChoice || initialMultipleChoiceAnswer
+        ).selectedOptionIndex;
         if (currentSelected !== OTHER_IDX && value.length > 0) {
-          state.questions[questionIdx].answer.multipleChoice!.selectedOptionIndex = OTHER_IDX;
+          (
+            state.questions[questionIdx].answer.multipleChoice || initialMultipleChoiceAnswer
+          ).selectedOptionIndex = OTHER_IDX;
         }
       }
-      if (state.questions[questionIdx].answer.checkboxes) {
-        state.questions[questionIdx].answer.checkboxes!.other = value;
 
-        const currentSelected =
-          state.questions[questionIdx].answer.checkboxes!.selectedOptionIndexes;
+      if (state.questions[questionIdx].layout.type === QUESTION_TYPES.checkboxes) {
+        (state.questions[questionIdx].answer.checkboxes || initialCheckboxesAnswer).other = value;
+
+        const currentSelected = (
+          state.questions[questionIdx].answer.checkboxes || initialCheckboxesAnswer
+        ).selectedOptionIndexes;
+
         if (!currentSelected.includes(OTHER_IDX) && value.length > 0) {
-          state.questions[questionIdx].answer.checkboxes!.selectedOptionIndexes.push(OTHER_IDX);
+          (
+            state.questions[questionIdx].answer.checkboxes || initialCheckboxesAnswer
+          ).selectedOptionIndexes.push(OTHER_IDX);
         }
       }
     },
