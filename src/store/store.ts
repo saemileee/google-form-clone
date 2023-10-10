f517accd661517201da6362cf02f1f1b8823a19c
샘 <68241138+saemileee@users.noreply.github.com>
2023-10-10 12:50:07 +0900

    remove: 결과 상태 브라우저 스토리지 이용함에 따라 리덕스 스토어 삭제

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit';
 import questionFormReducer from '../features/surveyPostSlice';
 import surveyPreviewFormSlice from '../features/surveyPreviewFormSlice';
-import surveyResultSlice from '../features/surveyResultSlice';
 
 export const store = configureStore({
   reducer: {
     questionForm: questionFormReducer,
     surveyPreviewForm: surveyPreviewFormSlice,
-    surveyResult: surveyResultSlice,
   },
 });
 
