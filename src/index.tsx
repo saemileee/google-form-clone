c9fb04087b1c422766a4d892d1b9ab2a248dfc7c
샘 <68241138+saemileee@users.noreply.github.com>
2023-10-10 17:13:30 +0900

    remove: 불필요한 파일 삭제

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './reset.css';
-import reportWebVitals from './reportWebVitals';
 import {Router} from './Router';
 import {RouterProvider} from 'react-router-dom';
 import {Provider} from 'react-redux';
@@ -15,8 +14,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
