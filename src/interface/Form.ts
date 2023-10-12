f3e6a4fe0207834fa9f87ba4421cc360a4fd5c00
샘 <68241138+saemileee@users.noreply.github.com>
2023-10-12 18:22:59 +0900

    refactor: Form 인터페이스 변경 - DefaultQuestion 추가 - Question.id 추가 - Question.isSelected => Question.isFocused 변경 - Question.type에 따른 Question 인터페이스 구분

--- src/interface/Form.ts
+++ src/interface/Form.ts
@@ -10,15 +10,6 @@ export type QuestionType = (typeof QUESTION_TYPES)[keyof typeof QUESTION_TYPES];
 
 export type OptionType = string;
 
-export interface Question {
-  isSelected: boolean;
-  title: QuestionTitle;
-  type: QuestionType;
-  options: OptionType[];
-  isOtherSelected: boolean;
-  isRequired: boolean;
-}
-
 export interface Form {
   title: FormTitle;
   description: FormDescription;
@@ -65,4 +56,53 @@ export interface PreviewQuestionForm {
   questions: PreviewQuestion[];
   invalidQuestions: number[];
   submitTryCount: number;
+}
+
+// Option
+export interface Option {
+  id: string;
+  value: string;
+  isSelected: boolean;
+}
+
+// Question
+interface DefaultQuestion {
+  id: string;
+  type: QuestionType;
+  title: QuestionTitle;
+  isRequired: boolean;
+  isFocused: boolean;
+}
+
+export interface MultipleChoice extends DefaultQuestion {
+  options: Option[];
+  isOtherSelected: boolean;
+  other: string;
+}
+
+interface Checkboxes extends DefaultQuestion {
+  options: Option[];
+  isOtherSelected: boolean;
+  other: string;
+}
+
+interface DropDown extends DefaultQuestion {
+  options: Option[];
+}
+
+interface ShortAnswer extends DefaultQuestion {
+  answer: string;
+}
+
+interface Paragraph extends DefaultQuestion {
+  answer: string;
+}
+
+export type Question = MultipleChoice | Checkboxes | DropDown | ShortAnswer | Paragraph;
+
+// Form
+export interface SurveyForm {
+  title: FormTitle;
+  description: FormDescription;
+  questions: Question[];
 }
