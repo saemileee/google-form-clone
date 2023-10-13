a887beebfac4d40625893b0573717503b67fdd6d
샘 <68241138+saemileee@users.noreply.github.com>
2023-10-13 14:40:56 +0900

    feat: 재사용을 위한 initialForms 모듈화

---
+++ src/features/initialForms.ts
@@ -0,0 +1,70 @@
+import uuid from 'react-uuid';
+import {DEFAULT_VALUES, QUESTION_TYPES} from '../constants/Form';
+import {Option, MultipleChoice, SurveyForm, Checkboxes, DropDown} from '../interface/Form';
+
+export const initialOption: Option = {
+  id: uuid(),
+  value: `${DEFAULT_VALUES.QUESTION_OPTION} 1`,
+  isSelected: false,
+};
+
+export const initialOther = {
+  isFormActive: false,
+  isSelected: false,
+  value: '',
+};
+
+export const initialMultipleChoice: MultipleChoice = {
+  id: uuid(),
+  isFocused: true,
+  title: DEFAULT_VALUES.QUESTION_TITLE,
+  type: QUESTION_TYPES.multipleChoice,
+  options: [initialOption],
+  other: initialOther,
+  isRequired: false,
+};
+
+export const initialCheckboxes: Checkboxes = {
+  id: uuid(),
+  isFocused: true,
+  title: DEFAULT_VALUES.QUESTION_TITLE,
+  type: QUESTION_TYPES.checkboxes,
+  options: [initialOption],
+  other: initialOther,
+  isRequired: false,
+};
+
+export const initialDropDown: DropDown = {
+  id: uuid(),
+  isFocused: true,
+  title: DEFAULT_VALUES.QUESTION_TITLE,
+  type: QUESTION_TYPES.dropDown,
+  options: [initialOption],
+  isRequired: false,
+};
+
+export const initialShortAnswer = {
+  id: uuid(),
+  isFocused: true,
+  title: DEFAULT_VALUES.QUESTION_TITLE,
+  type: QUESTION_TYPES.shortAnswer,
+  answer: '',
+  isRequired: false,
+};
+
+export const initialParagraph = {
+  id: uuid(),
+  isFocused: true,
+  title: DEFAULT_VALUES.QUESTION_TITLE,
+  type: QUESTION_TYPES.paragraph,
+  answer: '',
+  isRequired: false,
+};
+
+export const initialQuestion = initialMultipleChoice;
+
+export const initialSurveyForm: SurveyForm = {
+  title: DEFAULT_VALUES.TITLE,
+  description: '',
+  questions: [initialQuestion],
+};
