e2866193e45f73478422561dd8b5c1a48de3fbfc
샘 <68241138+saemileee@users.noreply.github.com>
2023-10-14 17:20:44 +0900

    feat: 임시저장 시간 렌더링, 폼 저장, 클리어 버튼 및 기능 적용

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,71 @@
-import {AiOutlineEye} from 'react-icons/ai';
+import {AiOutlineClear, AiOutlineEye} from 'react-icons/ai';
+import {BsSave} from 'react-icons/bs';
 import styled from 'styled-components';
 import {StyledMenuButton} from '../styles/Form';
 import {color} from '../styles/variables.ts/color';
+import {useDispatch, useSelector} from 'react-redux';
+import {RootState} from '../store/store';
+import {clearPostForm, saveSurveyForm} from '../features/surveyPostSlice';
 
 const Header = () => {
+  const saveTime = useSelector((state: RootState) => state.questionForm.saveTime);
+
+  const dispatch = useDispatch();
+
+  const saveForm = () => {
+    dispatch(saveSurveyForm());
+  };
+
+  const clearForm = () => {
+    if (window.confirm('모든 폼이 초기화됩니다.')) {
+      dispatch(clearPostForm());
+    }
+  };
   const openPreviewTab = () => {
+    dispatch(saveSurveyForm());
     window.open(
       `${window.location.origin}${window.location.pathname}preview`,
       '_blank',
       'noopener, noreferrer'
     );
   };
+
   return (
     <StyledHeaderContainer>
-      <span>Survey form</span>
-      <StyledMenuButton
-        aria-label='preview-form'
-        name='preview'
-        $tooltipPosition='bottom'
-        onClick={openPreviewTab}
-      >
-        <AiOutlineEye size={24} />
-      </StyledMenuButton>
+      <div>
+        <h1>Survey form</h1>
+        {saveTime && (
+          <div>
+            <span>마지막 임시저장: {saveTime}</span>
+            <StyledMenuButton
+              aria-label='save'
+              name='save'
+              $tooltipPosition='bottom'
+              onClick={saveForm}
+            >
+              <BsSave size={18} />
+            </StyledMenuButton>
+          </div>
+        )}
+      </div>
+      <div>
+        <StyledMenuButton
+          aria-label='clear-all'
+          name='clear'
+          $tooltipPosition='bottom'
+          onClick={clearForm}
+        >
+          <AiOutlineClear size={24} />
+        </StyledMenuButton>
+        <StyledMenuButton
+          aria-label='preview-form'
+          name='preview'
+          $tooltipPosition='bottom'
+          onClick={openPreviewTab}
+        >
+          <AiOutlineEye size={24} />
+        </StyledMenuButton>
+      </div>
     </StyledHeaderContainer>
   );
 };
@@ -39,8 +83,20 @@ const StyledHeaderContainer = styled.header`
   width: 100vw;
   background-color: white;
   box-shadow: 0 0 4px ${color.lightgrey};
-  span {
-    font-size: 15pt;
-    font-weight: 600;
+  div {
+    display: flex;
+    align-items: center;
+    gap: 24px;
+    h1 {
+      font-size: 15pt;
+      font-weight: 600;
+    }
+    div {
+      display: flex;
+      gap: 12px;
+      span {
+        font-size: 10pt;
+      }
+    }
   }
 `;
