ac6f1d28612ab473f17832b251a759eeca8b63bf
샘 <68241138+saemileee@users.noreply.github.com>
2023-10-15 18:31:57 +0900

    feat: 버튼 클릭 시 메인으로 이동

--- src/containers/NotFound.tsx
+++ src/containers/NotFound.tsx
@@ -2,10 +2,13 @@ import styled from 'styled-components';
 import {color} from '../styles/variables.ts/color';
 
 const NotFound = () => {
+  const navigateToMain = () => {
+    window.location.href = '/';
+  };
   return (
     <NotFoundContainer>
       <h1>😭 잘못된 접근입니다...</h1>
-      <p>메인 화면으로 이동합니다..</p>
+      <button onClick={navigateToMain}>메인 화면으로 이동</button>
     </NotFoundContainer>
   );
 };
@@ -17,15 +20,21 @@ const NotFoundContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-self: center;
+  align-items: center;
   text-align: center;
-  gap: 30px;
+  gap: 50px;
   h1 {
     font-size: 48px;
     font-weight: 600;
     color: ${color.primary};
   }
-  p {
+  button {
+    width: fit-content;
+    padding: 12px;
+    border-radius: 12px;
+    background-color: ${color.primary};
     font-size: 24px;
+    color: white;
+    cursor: pointer;
   }
 `;
